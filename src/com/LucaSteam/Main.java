05499b05c8be2c3c572f8a195c4e97943f16759e
ruugii_31 <90140860+ruugii@users.noreply.github.com>
2023-08-02 17:28:58 +0200

    write document

--- src/com/LucaSteam/Main.java
+++ src/com/LucaSteam/Main.java
@@ -4,6 +4,6 @@ import utilities.DocumentRead;
 
 public class Main {
     public static void main(String[] args) {
-        DocumentRead.read("src/resources/files/data.csv");
+        DocumentRead.SaveData("src/resources/files/data.csv", DocumentRead.InicialData("src/resources/files/data.csv"));
     }
 }
