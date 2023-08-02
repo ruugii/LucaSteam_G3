5161448280c7339821dc9d7a2aaa820f6855f915
ruugii_31 <90140860+ruugii@users.noreply.github.com>
2023-08-02 18:47:22 +0200

    Create Array with initial data

--- src/com/LucaSteam/Main.java
+++ src/com/LucaSteam/Main.java
@@ -1,9 +1,10 @@
 package com.LucaSteam;
 
+import services.GameService;
 import utilities.DocumentRead;
 
 public class Main {
     public static void main(String[] args) {
-        DocumentRead.SaveData("src/resources/files/data.csv", DocumentRead.InicialData("src/resources/files/data.csv"));
+        GameService.InicialData(DocumentRead.InicialData("src/resources/files/data.csv"));
     }
 }
