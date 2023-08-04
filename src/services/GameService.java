024f4941828f3654a43e101881493f4175857db6
ruugii_31 <90140860+ruugii@users.noreply.github.com>
2023-08-04 10:32:38 +0200

    if to avoid errors if the String have no coma

--- src/services/GameService.java
+++ src/services/GameService.java
@@ -10,29 +10,30 @@ public class GameService {
     static ListGame lg = new ListGame();
     public static void InicialData(ArrayList<String> option) {
         for (String line : option) {
-            System.out.println(line);
             String[] hasComilllas = line.split("\"");
             String[] separateComa = line.split(",");
             try {
-                if (!separateComa[1].contains("\"")){
-                    String[] aux = line.split(",");
-                    if (aux[3].equalsIgnoreCase("N/A")) {
-                        lg.addGame(CreateObjectGame.createGame(aux[1], aux[2], 0, aux[4], aux[5]));
+                if (line.contains(",") && line.split(",").length >= 5) {
+                    if (!separateComa[1].contains("\"")) {
+                        String[] aux = line.split(",");
+                        if (aux[3].equalsIgnoreCase("N/A")) {
+                            lg.addGame(CreateObjectGame.createGame(aux[1], aux[2], 0, aux[4], aux[5]));
+                        } else {
+                            lg.addGame(CreateObjectGame.createGame(aux[1], aux[2], Integer.parseInt(aux[3]), aux[4], aux[5]));
+                        }
                     } else {
-                        lg.addGame(CreateObjectGame.createGame(aux[1], aux[2], Integer.parseInt(aux[3]), aux[4], aux[5]));
-                    }
-                } else {
-                    String[] aux = hasComilllas[2].split(",");
-                    if (aux[2].equalsIgnoreCase("N/A")){
-                        lg.addGame(CreateObjectGame.createGame(
-                                "\"" + hasComilllas[1] + "\"",
-                                aux[1], 0, aux[3], aux[4]
-                        ));
-                    } else {
-                        lg.addGame(CreateObjectGame.createGame(
-                                "\"" + hasComilllas[1] + "\"",
-                                aux[1], Integer.parseInt(aux[2]), aux[3], aux[4]
-                        ));
+                        String[] aux = hasComilllas[2].split(",");
+                        if (aux[2].equalsIgnoreCase("N/A")) {
+                            lg.addGame(CreateObjectGame.createGame(
+                                    "\"" + hasComilllas[1] + "\"",
+                                    aux[1], 0, aux[3], aux[4]
+                            ));
+                        } else {
+                            lg.addGame(CreateObjectGame.createGame(
+                                    "\"" + hasComilllas[1] + "\"",
+                                    aux[1], Integer.parseInt(aux[2]), aux[3], aux[4]
+                            ));
+                        }
                     }
                 }
             } catch (Error e){
--- src/testing/TestRoger.java
+++ src/testing/TestRoger.java
@@ -8,6 +8,7 @@ import model.Game;
 import model.Publisher;
 
 import org.junit.jupiter.api.Test;
+import services.CreateObjectGame;
 import services.GameService;
 import utilities.DocumentRead;
 import utilities.MathOperation;
@@ -61,7 +62,28 @@ public class TestRoger {
                         )
                 ).getGen(), gen
         );
+    }
 
+    @Test
+    void isAnArray(){
+        GameService.InicialData(DocumentRead.InicialData("src/resources/files/data.csv"));
+        Genre [] GeP = { Genre.PLATFORM, Genre.SPORTS, Genre.RACING, Genre.SHOOTER, Genre.MISC, Genre.ACTION, Genre.ROLEPLAYING, Genre.PUZZLE, Genre.FIGHTING, Genre.STRATEGY, Genre.ADVENTURE, Genre.SIMULATION, Genre.DONT_EXIST};
+        Genre gen = GeP[MathOperation.getRandomNumber(0, GeP.length)];
+        assertEquals(GameService.sortGen(gen).getClass(), new ArrayList<>().getClass());
+    }
+
+    @Test
+    void isOk() {
+        Game game1 = CreateObjectGame.createGame("Draco", "xb" , 2002, "action", "bethesda");
+        Game game2 = new Game("Draco", Platform.XB , 2002, Genre.ACTION, new Publisher("bethesda"));
+        assertEquals(game1.getValue(), game2.getValue());
+    }
+
+    @Test
+    void isOkCreate() {
+        Game game1 = CreateObjectGame.createGame("Draco", Platform.XB , 2002, Genre.ACTION, new Publisher("bethesda"));
+        Game game2 = new Game("Draco", Platform.XB , 2002, Genre.ACTION, new Publisher("bethesda"));
+        assertEquals(game1.getValue(), game2.getValue());
     }
     
 }
